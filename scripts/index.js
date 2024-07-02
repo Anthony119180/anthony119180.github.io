a702c99ad6240939d22723f67a89231f026c06c3
NickZQ <1191806788@qq.com>
2024-07-02 21:50:19 +0800

    渐显

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+// document.addEventListener('DOMContentLoaded',function(){
+//     const items = document.querySelectorAll('.fade-in');
+//     items.forEach(function(item){
+//         item.classList.add('fade-in');
+//     })
+// });
